675dcfab810715bc97d9d367466a81cecd6067fa
abhijeetanandshah6997 <abhijeet.shah@nineleaps.com>
2021-12-23 13:36:53 +0530

    Initial Commit

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='netsuite_sdk_py',
-    version = '1.1.1',
+    version = '1.1.1.0',
     package_dir={"netsuitesdk": "netsuitesdk"},
     packages = ['netsuitesdk'],
     author = 'Precanto',
@@ -15,9 +15,9 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url = 'https://github.com/Precanto/py-netsuite-sdk',
-    download_url = 'https://github.com/Precanto/py-netsuite-sdk/archive/refs/tags/1.1.1.tar.gz',
+    download_url = 'https://github.com/Precanto/py-netsuite-sdk/archive/refs/tags/1.1.1.0.tar.gz',
     keywords=['netsuite', 'api', 'python', 'sdk'],
-    install_requires=["python-zeep", "requests"],
+    install_requires=["zeep==3.4.0", "requests"],
     include_package_data=True,
     python_requires=">=3.6",
     classifiers = [
