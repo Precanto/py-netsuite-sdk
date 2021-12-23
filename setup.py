f9abb00749874633f2244a1907ea391082c05722
abhijeetanandshah6997 <abhijeet.shah@nineleaps.com>
2021-12-23 13:10:14 +0530

    Initial Commit

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='netsuite_sdk_py',
-    version = '1.0.1',
+    version = '1.0.2',
     package_dir={"netsuitesdk": "netsuitesdk"},
     packages = ['netsuitesdk'],
     author = 'Precanto',
